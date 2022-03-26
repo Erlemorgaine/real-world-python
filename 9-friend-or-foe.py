6b3f395e81f41ded137f09f6d0c312989eb62a68
Erle Monfils <erlemonfils@TVA053.wind3.hub>
2022-03-26 14:32:28 +0100

    WIP friend or foe

--- 9-friend-or-foe.py
+++ 9-friend-or-foe.py
@@ -9,10 +9,13 @@ import cv2 as cv
 
 # Engine is a name by convention
 engine = pyttsx3.init()
-engine.setProperty('rate', 145)
+voices = engine.getProperty('voices')
+engine.setProperty('rate', 145) # Rate of speech
 engine.setProperty('volume', 1.0)
+engine.setProperty('voice', voices[1])
 
 root_dir = os.path.abspath('.')
+tone_path = os.path.join(root_dir, 'tone.wav')
 # haarfilters_path = 'https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/'
 
 # facehaar = requests.get(haarfilters_path + 'haarcascade_frontalface_default.xml')
@@ -26,6 +29,34 @@ eye_cascade = cv.CascadeClassifier(root_dir + 'haarcascade_eye.xml')
 
 os.chdir('corridor_5')
 contents = sorted(os.listdir())
+
+for image in contents:
+    print(f"Motion detected at {datetime.now()}")
+    discharge_weapon = True
+    engine.say("You have entered an active fire zone. \
+        Face the machine! \
+        You have 5 seconds.")
+
+    # This halts program execution, flushes say() queue and plays the audio
+    engine.runAndWait()
+    time.sleep(3)
+
+    # This simulates making a capture of the video stream.
+    # There is not really a reason to make the image grey, except the author liked to show a grey image.
+    # Actually opencv only works with greyscale images, but converts them itself behind the scenes
+    img_gray = cv.imread(image, cv.IMREAD_GRAYSCALE)
+    height, width = img_gray.shape
+
+    # Showing the image is for quality control, to make sure all the images are being used
+    cv.imshow(f'Motion detected {image}', img_gray)
+    cv.waitKey(2000)
+    cv.destroyWindow(f'Motion detected {image}')
+
+    # The items in this list will be the x, y, width height of the rectangle from the image
+    face_rect_list = []
+    face_rect_list.append(face_cascade.detectMultiScale(image=img_gray,
+        scaleFactor=1.1,
+        minNeighbors=5))
 
 
 
